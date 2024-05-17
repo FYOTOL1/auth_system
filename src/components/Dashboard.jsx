c9e40757b6007792614fe4849b8bd243a57c3ed1
Ahmed <fyotol@gmail.com>
2024-05-17 12:03:34 -0700

    Edit: change Element At Dashboard | 5/17/2024 | 20s

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Layout from "../Layout";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const Logout = () => {
+    localStorage.removeItem("user_info");
+    navigate("/login");
+  };
+
   return (
     <>
       <Layout>
@@ -9,6 +16,7 @@ const Dashboard = () => {
           <header className="flex justify-center items-center gap-5 h-10 w-full bg-gray-100 shadow-md shadow-gray-300">
             <Link to={"/login"}>Login</Link>
             <Link to={"/register"}>Register</Link>
+            <button onClick={() => Logout()}>Logout</button>
           </header>
           <h1 className="text-4xl text-blue-400">Dashboard</h1>
         </div>
