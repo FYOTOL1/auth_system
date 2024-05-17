3aedbf8e632397d0fab6203b7f90798c40c5643d
Ahmed <fyotol@gmail.com>
2024-05-17 12:00:50 -0700

    Edit: change Element At Dashboard | 5/17/2024 | 20s

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,11 +5,13 @@ const Dashboard = () => {
   return (
     <>
       <Layout>
-        <header className="flex justify-center items-center gap-5 h-10 w-full bg-gray-50 shadow-sm">
-          <Link to={"/login"}>Login</Link>
-          <Link to={"/register"}>Register</Link>
-        </header>
-        <div className="text-4xl text-blue-400">Dashboard</div>
+        <div className="flex flex-col justify-between items-center gap-20 w-full">
+          <header className="flex justify-center items-center gap-5 h-10 w-full bg-gray-100 shadow-md shadow-gray-300">
+            <Link to={"/login"}>Login</Link>
+            <Link to={"/register"}>Register</Link>
+          </header>
+          <h1 className="text-4xl text-blue-400">Dashboard</h1>
+        </div>
       </Layout>
     </>
   );
