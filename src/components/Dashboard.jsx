318eb1e341cbf4da20adf99061180497bb3e39cb
Ahmed <fyotol@gmail.com>
2024-05-17 11:44:24 -0700

    Add: Add Header For Test Another Pages | 5/17/2024 | 1m

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
 import Layout from "../Layout";
 
 const Dashboard = () => {
   return (
     <>
       <Layout>
+        <header className="flex justify-center items-center gap-5 h-10 w-full bg-gray-50 shadow-sm">
+          <Link to={"/login"}>Login</Link>
+          <Link to={"/register"}>Register</Link>
+        </header>
         <h1 className="text-4xl text-blue-400">Dashboard</h1>
       </Layout>
     </>
