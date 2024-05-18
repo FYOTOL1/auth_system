9b7bd92f7d43bc094297d3c50093a27857fafd92
Ahmed <fyotol@gmail.com>
2024-05-17 17:21:02 -0700

    Add: Add Advanced Validation Logic | 5/17/2024 | 10m

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { RxDotFilled } from "react-icons/rx";
 import { IoMdEyeOff } from "react-icons/io";
 import { IoMdEye } from "react-icons/io";
@@ -11,6 +11,7 @@ const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [Show_Password, setShow_Password] = useState(false);
+  const [CanSubmit, setCanSubmit] = useState(false);
 
   const navigate = useNavigate();
 
@@ -119,6 +120,18 @@ const Register = () => {
     }
   };
 
+  useEffect(() => {
+    if (
+      password_required_List.every((f) => f.check == true) &&
+      username.length &&
+      email.length
+    ) {
+      setCanSubmit(true);
+    } else {
+      setCanSubmit(false);
+    }
+  }, [email.length, password, password_required_List, username.length]);
+
   return (
     <>
       <ToastContainer
@@ -136,90 +149,100 @@ const Register = () => {
       />
 
       <div className="flex justify-between gap-3 w-full min-h-screen h-full outline outline-1 outline-gray-100 rounded-md">
-        <div className="w-full sm:w-[55%]">
-          <img className="w-40" src="/logo.png" alt="Logo" />
-          <div className="px-2 sm:px-16">
-            <h1 className="mt-10 text-4xl font-sans text-zinc-700 font-semibold">
-              Welcome to Learn2Earn
-            </h1>
-            <p className="text-sm mt-2 text-zinc-800">
-              Already have an Account?
-              <Link className="underline text-black ms-1" to="/login">
-                Log in
-              </Link>
-            </p>
-
-            <form
-              onSubmit={(s) => s.preventDefault()}
-              className="mt-8 w-full max-w-[700px]"
-            >
-              <div className="flex flex-col gap-2 w-full text-zinc-500">
-                <label htmlFor="email">Email</label>
-                <input
-                  onChange={(c) => setEmail(c.target.value)}
-                  id="email"
-                  className="outline outline-1 outline-gray-300 px-4 py-3 rounded-lg text-zinc-800 transition-all focus:outline-gray-700"
-                  type="text"
-                  required
-                  name="email"
-                  value={email}
-                />
-              </div>
-
-              <div className="flex flex-col gap-2 w-full text-zinc-500 mt-6">
-                <label htmlFor="username">Username</label>
-                <input
-                  onChange={(c) => setUsername(c.target.value)}
-                  id="username"
-                  className="outline outline-1 outline-gray-300 px-4 py-3 rounded-lg text-zinc-800 transition-all focus:outline-gray-700"
-                  type="text"
-                  required
-                  name="username"
-                  value={username}
-                />
-              </div>
-
-              <div className="flex flex-col gap-2 w-full text-zinc-500 mt-6">
-                <label
-                  className="flex justify-between items-center"
-                  htmlFor="password"
-                >
-                  <p>Password</p>
-                  <button
-                    onClick={() => setShow_Password(!Show_Password)}
-                    className="flex gap-1 items-center"
-                  >
-                    {show_un_password()}
-                  </button>
-                </label>
-                <input
-                  onChange={(c) => setPassword(c.target.value)}
-                  id="password"
-                  className="outline outline-1 outline-gray-300 px-4 py-3 rounded-lg text-zinc-800 transition-all focus:outline-gray-700"
-                  type={Show_Password ? "text" : "password"}
-                  value={password}
-                  required
-                  name="password"
-                />
-                <ul className="flex gap-x-10 gap-y-2 flex-wrap">
-                  {render_required()}
-                </ul>
-              </div>
-
-              <p className="mt-7 text-zinc-700">
-                By creating an account, you agree to the{" "}
-                <span className="underline text-zinc-700">Terms of use</span>{" "}
-                and{" "}
-                <span className="underline text-zinc-700">Privacy Policy</span>.
+        <div className="flex flex-col items-end w-full sm:w-[55%] lg:container ms-auto p-3">
+          <div>
+            <img className="w-40" src="/logo.png" alt="Logo" />
+            <div className="px-2 sm:px-16">
+              <h1 className="mt-10 text-4xl font-sans text-zinc-700 font-semibold">
+                Welcome to Learn2Earn
+              </h1>
+              <p className="text-sm mt-2 text-zinc-800">
+                Already have an Account?
+                <Link className="underline text-black ms-1" to="/login">
+                  Log in
+                </Link>
               </p>
 
-              <button
-                onClick={() => RegisterFunc()}
-                className="py-4 px-10 rounded-full bg-gray-400 text-white mt-10 transition-all hover:bg-zinc-900"
+              <form
+                onSubmit={(s) => s.preventDefault()}
+                className="mt-8 w-full max-w-[700px]"
               >
-                create an account
-              </button>
-            </form>
+                <div className="flex flex-col gap-2 w-full text-zinc-500">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    onChange={(c) => setEmail(c.target.value)}
+                    id="email"
+                    className="outline outline-1 outline-gray-300 px-4 py-3 rounded-lg text-zinc-800 transition-all focus:outline-gray-700"
+                    type="text"
+                    required
+                    name="email"
+                    value={email}
+                  />
+                </div>
+
+                <div className="flex flex-col gap-2 w-full text-zinc-500 mt-6">
+                  <label htmlFor="username">Username</label>
+                  <input
+                    onChange={(c) => setUsername(c.target.value)}
+                    id="username"
+                    className="outline outline-1 outline-gray-300 px-4 py-3 rounded-lg text-zinc-800 transition-all focus:outline-gray-700"
+                    type="text"
+                    required
+                    name="username"
+                    value={username}
+                  />
+                </div>
+
+                <div className="flex flex-col gap-2 w-full text-zinc-500 mt-6">
+                  <label
+                    className="flex justify-between items-center"
+                    htmlFor="password"
+                  >
+                    <p>Password</p>
+                    <button
+                      onClick={() => setShow_Password(!Show_Password)}
+                      className="flex gap-1 items-center"
+                    >
+                      {show_un_password()}
+                    </button>
+                  </label>
+                  <input
+                    onChange={(c) => setPassword(c.target.value)}
+                    id="password"
+                    className="outline outline-1 outline-gray-300 px-4 py-3 rounded-lg text-zinc-800 transition-all focus:outline-gray-700"
+                    type={Show_Password ? "text" : "password"}
+                    value={password}
+                    required
+                    name="password"
+                  />
+                  <ul className="flex gap-x-10 gap-y-2 flex-wrap">
+                    {render_required()}
+                  </ul>
+                </div>
+
+                <p className="mt-7 text-zinc-700">
+                  By creating an account, you agree to the{" "}
+                  <span className="underline text-zinc-700">Terms of use</span>{" "}
+                  and{" "}
+                  <span className="underline text-zinc-700">
+                    Privacy Policy
+                  </span>
+                  .
+                </p>
+
+                <button
+                  disabled={!CanSubmit}
+                  onClick={() => RegisterFunc()}
+                  className={`py-4 px-10 rounded-full text-white mt-10 transition-all ${
+                    CanSubmit
+                      ? "hover:bg-zinc-900 bg-blue-400 cursor-pointer"
+                      : "bg-gray-400"
+                  }`}
+                >
+                  create an account
+                </button>
+              </form>
+            </div>
           </div>
         </div>
 
